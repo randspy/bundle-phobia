8ecfbc6dc955ba6dbdf725e8f8fb12d93cd234ec
Przemyslaw Koziel <mos.koziel@gmail.com>
2020-02-08 18:55:01 +0100

    Added axios, react-router-dom, react-select and support for async functions

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['@babel/plugin-transform-runtime']
         }
       },
       // New rules to load css
