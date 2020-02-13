c7a397fb23c33875312b43558382aaadc31daddd
Przemyslaw Koziel <mos.koziel@gmail.com>
2020-02-13 10:56:07 +0100

    Result page

--- client/src/result/result.js
+++ client/src/result/result.js
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import BundleStats from './bundle-stats/bundle-stats';
+import fromApi from './from-api/from-api';
+import useQuery from './use-query/use-query';
+import fetchBundles from './fetch-bundles/fetch-bundles';
+import './result.css';
 
 export default function Result() {
-  return <div>Result</div>;
+  let query = useQuery();
+  let [bundle, setBundle] = useState({
+    minified: 0,
+    gzip: 0
+  });
+
+  useEffect(() => {
+    fetchBundles(query.name, query.version).then(result => {
+      const data = fromApi(result);
+      if (data.length) {
+        setBundle(data[data.length - 1]);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="result">
+      <Link to="/">
+        <div className="result--back-button">
+          BUNDLE<div className="result--secondary-back-button">PHOBIA</div>
+        </div>
+      </Link>
+      <div className="result--content">
+        <BundleStats minified={bundle.minified} gzip={bundle.gzip} />
+      </div>
+    </div>
+  );
 }
---
+++ client/src/result/result.spec.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import JestMockPromise from 'jest-mock-promise';
+import Result from './result';
+import BundleStats from './bundle-stats/bundle-stats';
+import { BrowserRouter as Router } from 'react-router-dom';
+import fetchBundles from './fetch-bundles/fetch-bundles';
+import useQuery from './use-query/use-query';
+
+jest.mock('./use-query/use-query');
+jest.mock('./fetch-bundles/fetch-bundles');
+
+describe('Result', () => {
+  test.skip('should wire component', () => {
+    const wrapper = shallow(<Result />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  test('should contain BundleStats with default values', () => {
+    const wrapper = shallow(<Result />);
+    expect(
+      wrapper.containsMatchingElement(<BundleStats minified={0} gzip={0} />)
+    ).toEqual(true);
+  });
+
+  test('should display fetched bundles', () => {
+    useQuery.mockReturnValue({
+      name: 'react',
+      version: '16.3.3'
+    });
+
+    fetchBundles.mockReturnValue(
+      new JestMockPromise(resolve => {
+        resolve({
+          '16.1.1': {
+            minified: 2048,
+            gzip: 1024
+          }
+        });
+      })
+    );
+    let wrapper = null;
+    act(() => {
+      wrapper = mount(
+        <Router>
+          <Result />
+        </Router>
+      );
+    });
+
+    wrapper.update();
+    expect(wrapper.find('.bundle-stats--minified').text()).toEqual('2kB');
+    expect(wrapper.find('.bundle-stats--gzip').text()).toEqual('1kB');
+    expect(fetchBundles).toHaveBeenCalledWith('react', '16.3.3');
+  });
+
+  test('should do nothing for empty bundle object', () => {
+    useQuery.mockReturnValue({
+      name: 'react',
+      version: '16.3.3'
+    });
+
+    fetchBundles.mockReturnValue(
+      new JestMockPromise(resolve => {
+        resolve({});
+      })
+    );
+    let wrapper = null;
+    act(() => {
+      wrapper = mount(
+        <Router>
+          <Result />
+        </Router>
+      );
+    });
+
+    wrapper.update();
+    expect(wrapper.find('.bundle-stats--minified').text()).toEqual('0kB');
+    expect(wrapper.find('.bundle-stats--gzip').text()).toEqual('0kB');
+    expect(fetchBundles).toHaveBeenCalledWith('react', '16.3.3');
+  });
+
+  it('should render correctly', () => {
+    const wrapper = shallow(<Result />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
